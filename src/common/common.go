abeb5264b3b124c79ad4561a9dae268f17bd2ed7
Josh MacDonald <josh.macdonald@gmail.com>
2013-03-09 01:27:04 -0800

    Add a scraper for wikipedia city coordinates

---
+++ src/common/common.go
@@ -0,0 +1,52 @@
+package common
+
+import "net/http"
+import "io/ioutil"
+import "time"
+
+const (
+	UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) " +
+		"AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17"
+)
+
+var (
+	client      *http.Client
+)
+
+func init() {
+	// Note: Microsoft ASP.NET has a bug in certain browser
+	// configurations which causes incorrect receipt of the
+	// ScriptResource.axd file.  E.g.,
+	// stackoverflow.com/questions/5681122/asp-net-ipad-safari-cache-issue
+	// Seems to be a problem _only_ when a proxy is involved.
+
+	client = &http.Client{
+		Transport: &http.Transport{Proxy: http.ProxyFromEnvironment,
+			DisableCompression: true},
+	}
+}
+
+func SleepAWhile(url, query string) {
+	time.Sleep(time.Second * 10)
+}
+
+func GetUrl(host, uri, query string) ([]byte, error) {
+	SleepAWhile(uri, query)
+	url := "http://" + host + uri + query
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", UserAgent)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
